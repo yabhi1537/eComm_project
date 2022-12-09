fdc55dcac2e5a1b4aaaaa836847ea9731650e832
Abhi Yadav <120159324+yabhi1537@users.noreply.github.com>
2022-12-09 11:27:03 +0530

    Add files via upload

---
+++ file.php
@@ -0,0 +1,19 @@
+<?php
+
+   echo $name;
+   echo  "<br>";
+   echo $email;
+   echo  "<br>";
+   echo $website;
+   echo  "<br>";
+   echo $comment;
+   echo  "<br>";
+   echo $gender;
+   echo  "<br>";
+
+
+
+
+
+ ?>
+    
